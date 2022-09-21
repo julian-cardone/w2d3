cf9a4d64a4d63457929660335b8755f4e33aca76
Julian Cardone <juliancardone1@gmail.com>
2022-09-21 11:03:50 -0400

    finished player class

--- battleship_project/lib/player.rb
+++ battleship_project/lib/player.rb
@@ -1,3 +1,9 @@
 class Player
 
+    def get_move
+        puts "enter a position with coordinates separated with a space like `4 7`"
+        nums = gets.chomp
+        return [nums[0].to_i, nums[2].to_i]
+    end
+
 end
