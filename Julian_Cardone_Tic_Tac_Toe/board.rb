199667d331816e4e192d57e7cd3da77f3ea449c0
Julian Cardone <juliancardone1@gmail.com>
2022-09-21 14:17:49 -0400

    working on tic tac toe

--- Julian_Cardone_Tic_Tac_Toe/board.rb
+++ Julian_Cardone_Tic_Tac_Toe/board.rb
@@ -26,4 +26,40 @@ class Board
         @grid[row][collumn] = mark
     end
 
+    def print
+        @grid.each {|arr| p arr}
+    end
+
+    def win_row?(mark)
+        @grid.each {|arr| return true if arr.count(mark) == 3}
+        false
+    end
+
+    def win_col?(mark)
+        @grid.transpose.each {|arr| return true if arr.count(mark) == 3}
+        false
+    end
+
+    def win_diagonal?(mark)
+        count = 0
+        @grid.each_with_index do |arr, i|
+            arr.each_with_index do |ele, j|
+                if i == j && @grid[i][j] == mark
+                    count += 1
+                end
+            end
+        end
+        return true if count == 3
+        count2 = 0
+        @grid.each_with_index do |arr, i|
+            arr.reverse.each_with_index do |ele, j|
+                if i == j && @grid[i][j] == mark
+                    count2 += 1
+                end
+            end
+        end
+        return true if count == 3 || count2 = 3
+        false
+    end
+
 end
