3e9a1462587c7af463925c190b9dfaceff0968d3
Julian Cardone <juliancardone1@gmail.com>
2022-09-21 10:57:05 -0400

    completed Board class

--- battleship_project/lib/board.rb
+++ battleship_project/lib/board.rb
@@ -25,6 +25,55 @@ class Board
         @grid.flatten.count {|ele| ele == :S}
     end
 
-    
+    def attack(pos)
+        if self[pos] == :S
+            self[pos] = :H
+            puts "you sunk my battleship!"
+            return true
+        else
+            self[pos] = :X
+            return false
+        end
+    end
+
+    def place_random_ships
+        while num_ships < (0.25 * @size)
+            random = rand_coord(@size)
+            self[random] = :S if self[random] != :S 
+        end
+    end
+
+    def rand_coord(size)
+        rand_row = rand(0..(Math.sqrt(@size)-1))
+        rand_collumn = rand(0..(Math.sqrt(@size)-1))
+        return [rand_row, rand_collumn]
+    end
+
+    def hidden_ships_grid
+        hidden = @grid
+      hidden.map do |arr|
+        arr.map do |ele|
+            ele = :N if ele == :S
+            ele
+        end
+    end
+    end
+
+    def self.print_grid(array)
+        array.each do |arr|
+            puts arr.join(" ")
+        end
+    end
+
+
+    def cheat()
+        Board.print_grid(@grid)
+    end
+
+    def print()
+        Board.print_grid(hidden_ships_grid)
+    end
+
+
 
 end
